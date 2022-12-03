8064360a5718836d3145af9c5438c19a9b80d458
LAPTOP-5LU5TS8U\Rohit <katiyarrohitoo7@gmail.com>
2022-12-03 08:49:29 +0530

    Changes

--- Api/AhliFans.WebApi/Area/Client/Coach/GetInfo/GetByIdRequest.cs
+++ Api/AhliFans.WebApi/Area/Client/Coach/GetInfo/GetByIdRequest.cs
@@ -6,6 +6,7 @@ namespace AhliFans.WebApi.Area.Client.Coach;
 
 public record GetByIdRequest
 {
+	
   public const string Route = $"{nameof(Areas.Client)}/Coach";
   [FromHeader] public int Coach { get; set; }
   public string Lang { get; set; } = Languages.Ar;
